6f1cceeb2d2f319fcea9352423eb9c5c2eb52c37
Jared Hill <jhill2@spsu.edu>
2015-09-29 02:53:45 -0400

    Working on rag doll replacement

---
+++ TheArchitect/Assets/Scripts/Character/ragDollReplace.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ragDollReplace : MonoBehaviour {
+
+	public GameObject playerBody;
+	public GameObject ragDoll;
+
+	void OnTriggerEnter (Collider coll)
+	{
+		if (coll.tag == "DetTest")
+		{
+			playerBody.SetActive(false);
+			ragDoll.SetActive (true);
+		}
+	}
+
+}
