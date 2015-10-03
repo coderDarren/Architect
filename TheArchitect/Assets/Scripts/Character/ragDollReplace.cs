df0f1f6313d5dd04d85568433c77170a8acd1861
Jared Hill <jhill2@spsu.edu>
2015-10-02 22:43:31 -0400

    Getting Ragdoll replace to work
    Rag doll replace example is functioning

--- TheArchitect/Assets/Scripts/Character/ragDollReplace.cs
+++ TheArchitect/Assets/Scripts/Character/ragDollReplace.cs
@@ -5,13 +5,44 @@ public class ragDollReplace : MonoBehaviour {
 
 	public GameObject playerBody;
 	public GameObject ragDoll;
+	public GameObject playerGun;
+	public float downTime;
+	private float originalValue;
+	public bool timerRunning;
+
+	void Start ()
+	{
+		float originalValue = downTime;
+	}
+
+	void Update()
+	{
+		if (timerRunning)
+		{
+			downTime -= Time.deltaTime;
+			if (downTime <= 0)
+			{
+				ReinstatePlayerBody();
+			}
+		}
+	}
+
+	public void ReinstatePlayerBody()
+	{
+		ragDoll.SetActive(false);
+		playerBody.SetActive(true);
+		playerGun.SetActive(true);
+		downTime = originalValue;
+	}
 
 	void OnTriggerEnter (Collider coll)
 	{
 		if (coll.tag == "Fireball")
 		{
+			playerGun.SetActive(false);
 			playerBody.SetActive(false);
 			ragDoll.SetActive (true);
+			timerRunning = true;
 		}
 	}
 
